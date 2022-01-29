79eacc451a11916472031b24ceb4504433310e62
witherpearl <83099794+witherpearl@users.noreply.github.com>
2022-01-29 14:51:39 -0600

    Name change

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
+	name: "The Loading Tree",
 	id: "mymod",
-	author: "nobody",
+	author: "witherpearl",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
