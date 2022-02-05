1e138227ea1c86e65fa0dc93ef853775ae09069c
witherpearl <83099794+witherpearl@users.noreply.github.com>
2022-02-05 16:39:46 -0600

    Test

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
-addLayer("b", {
-    name: "Basic Ads", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "B", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("i", {
+    name: "Infection", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "I", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -9,20 +9,9 @@ addLayer("b", {
     color: "#ff4d4d",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "Ad coins", // Name of prestige currency
-    buyables: {
-        12: {
-            cost(x) { return new Decimal(2).mul(2) },
-            display() { return "Pass out paper ads" },
-            canAfford() { return player[this.layer].points.gte(this.cost()) },
-            buy() {
-                player[this.layer].points = player[this.layer].points.sub(this.cost())
-                setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))
-            },
-        },
-    },
      upgrades: {
         11: {
-            title: "Sticky notes",
+            title: "",
             description: "Double your point gain.",
             cost: new Decimal(1),
         },
